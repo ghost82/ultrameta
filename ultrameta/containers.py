393b45d42535e029a6b66e0b96ced0ddbd52b936
nicolas.minutillo <nicolas.minutillo@8e4a23b9-e14e-0410-9f2d-2f2a39760a57>
2008-06-30 12:26:17 +0000

    Code clean up and test changes.

--- ultrameta/containers.py
+++ ultrameta/containers.py
@@ -3,7 +3,7 @@
 from utils import replace_none
 from itertools import izip
 
-def _invariant_checked(self, func):
+def InvariantChecked(self, func):
     
     def wrapper(val):
         func(val)
@@ -11,9 +11,9 @@ def _invariant_checked(self, func):
         
     return wrapper
     
-class _withMixin(object):
+class WithMixin(object):
 
-    def _with(self, type_definition = None, invariant_checks = None):
+    def __with(self, type_definition = None, invariant_checks = None):
         self._type_definition = replace_none(getattr(self, '_type_definition', None), type_definition)
         self._invariant_checks = replace_none(getattr(self, '_invariant_checks', None), invariant_checks)
         
@@ -23,112 +23,54 @@ class _withMixin(object):
                 setattr(self, method, proxy)
         if self._invariant_checks:
             for method in methods:
-                setattr(self, method, _invariant_checked(self, getattr(self, method)))
+                setattr(self, method, InvariantChecked(self, getattr(self, method)))
 
         return self             
 
     def withtype(self, type_definition):
         if not type_definition.type_match(self):
             raise TypeError()
-        return self._with(type_definition = type_definition)
+        return self.__with(type_definition = type_definition)
         
     def withinvariants(self, invariant_checks):
         invariant_checks()
-        return self._with(invariant_checks = invariant_checks)
+        return self.__with(invariant_checks = invariant_checks)
         
     def proxy_definitions(self):
-        # this is meant to be overridden
-        return [], []
-
-class list_proxy(list, _withMixin):
+        raise NotImplementedError()
+        
+class ListProxy(list, WithMixin):
 
     def proxy_definitions(self):
-        methods = ['append', '__setslice__']
-        proxies = [self._append_type_safe, self._setslice_type_safe]
-        return methods, proxies
+        return ['append', '__setslice__'], [self.__append_type_safe, self.__setslice_type_safe]
 
-    def _append_type_safe(self, val):
+    def __append_type_safe(self, val):
         if self._type_definition.contents_match(val, None):
-            super(list_proxy, self).append(val)
+            super(ListProxy, self).append(val)
         else:
             raise TypeError('%s is a %s and cannot be appended to a %s' % 
                 (val, type(val), self._type_definition))
 
-    def _setslice_type_safe(self, i, j, val):
+    def __setslice_type_safe(self, i, j, val):
         if self._type_definition.type_match(val):
-            return super(list_proxy, self).__setslice__(i, j, val)
+            return super(ListProxy, self).__setslice__(i, j, val)
         else:
             raise TypeError('%s is not a %s' % (val, self._type_definition))        
 
             
-class tuple_proxy(tuple, _withMixin):
-    pass
-    
-class dictionary_proxy(dict, _withMixin):
+class TupleProxy(tuple, WithMixin):
 
     def proxy_definitions(self):
-        methods = ['__setitem__']
-        proxies = [self._setitem_type_safe]
-        return methods, proxies
+        return [], []
+    
+class DictionaryProxy(dict, WithMixin):
 
-    def _setitem_type_safe(self, key, val):
+    def proxy_definitions(self):
+        return ['__setitem__'], [self.__setitem_type_safe]
+
+    def __setitem_type_safe(self, key, val):
         if self._type_definition.contents_match(val, key):
-            super(dictionary_proxy, self).__setitem__(key, val)
+            super(DictionaryProxy, self).__setitem__(key, val)
         else:
             raise TypeError('%s is not a %s' % ((key, val), self._type_definition))
-        
-if __name__ == '__main__':
-    import unittest
-    from type_definition import _type_definition
-    
-    class Tests(unittest.TestCase):
-        
-        def append(list, val):
-            list.append(val)
-            
-        def assign(map, key, val):
-            map[key] = val
-        
-        def test_list_of_int(self):
-            type_definition = _type_definition([int])
-            test_list = list_proxy([1, 2, 3]).withtype(type_definition)
-            self.assertEqual(test_list, [1, 2, 3])
-            self.assertEqual(test_list[1], 2)
-            self.assertEqual(len(test_list), 3)
-           
-        def test_type_safe_list_of_int(self):
-            type_definition = _type_definition([int])
-            test_list = list_proxy([]).withtype(type_definition)
-            self.assertRaises(TypeError, self.append, test_list, 'a')
-            
-        def test_map_from_str_to_int(self):
-            type_definition = _type_definition({str:int})
-            test_map = dictionary_proxy({'a' : 1, 'b' : 2}).withtype(type_definition)
-            self.assertEqual(test_map['a'], 1)
-            test_map = dictionary_proxy({'a' : 1, 'b' : 'c'})
-            self.assertRaises(TypeError, test_map.withtype, type_definition)
-            
-        def test_type_safe_map_from_str_to_int(self):
-            type_definition = _type_definition({str:int})
-            test_map = dictionary_proxy({}).withtype(type_definition)
-            self.assertRaises(TypeError, self.assign, test_map, 1, 1)
-            self.assertRaises(TypeError, self.assign, test_map, 'a', 'a')
-            
-        def test_tuple_int_int_str_int(self):
-            type_definition = _type_definition((int, int, str, int))
-            test_tuple = tuple_proxy((1, 2, 'c', 3)).withtype(type_definition)
-            self.assertEqual(test_tuple, (1, 2, 'c', 3))
-            self.assertEqual(test_tuple[1], 2)
-            self.assertRaises(TypeError, self.assign, test_tuple, 2, 4)
-            
-        def test_tuple_type_safety(self):
-            type_definition = _type_definition((int, int, str, int))
-            test_tuple = tuple_proxy((1, 2, 3))
-            self.assertRaises(TypeError, test_tuple.withtype, type_definition)
-            
-            
-    suite = unittest.TestLoader().loadTestsFromTestCase(Tests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
-                        
             
--- ultrameta/magic.py
+++ ultrameta/magic.py
@@ -4,7 +4,7 @@ import type_definition
 from types import FunctionType
 from utils import replace_none
     
-class _descriptor(object):
+class Descriptor(object):
 
     def __init__(self, reader = None, writer = None):
         self._reader = reader
@@ -26,18 +26,18 @@ class _descriptor(object):
     def __delete__(self, instance):
         pass
 
-class _ordered_class_attr(object):
+class OrderedClassAttribute(object):
     order = 0
     
     def __init__(self):
-        self._order = _ordered_class_attr.order
-        _ordered_class_attr.order += 1
+        self._order = OrderedClassAttribute.order
+        OrderedClassAttribute.order += 1
         
-class _ultra_property(_ordered_class_attr):
+class UltraProperty(OrderedClassAttribute):
 
     def __init__(self):
-        super(_ultra_property, self).__init__()
-        self._type_definition = type_definition._type_definition(None)
+        super(UltraProperty, self).__init__()
+        self._type_definition = type_definition.TypeDefinition(None)
         
     @property
     def meta_data(self):
@@ -50,14 +50,14 @@ class _ultra_property(_ordered_class_attr):
         raise NotImplementedError()
         
     def create_property(self, attr):
-        return _descriptor(self.create_read_method(attr), self.create_write_method(attr))
+        return Descriptor(self.create_read_method(attr), self.create_write_method(attr))
     
 
-class _property(_ultra_property):
+class Property(UltraProperty):
 
     def __init__(self, prototype):
-        super(_property, self).__init__()
-        self._type_definition = type_definition._type_definition(prototype)
+        super(Property, self).__init__()
+        self._type_definition = type_definition.TypeDefinition(prototype)
         
     def create_read_method(self, attr):
         return lambda instance: instance.__dict__[attr]
@@ -72,10 +72,10 @@ class _property(_ultra_property):
             instance.__ultra_do_invariant_checks__()
         return writer
         
-class _identity(_property):
+class Identity(Property):
 
     def __init__(self, prototype):
-        super(_identity, self).__init__(prototype)
+        super(Identity, self).__init__(prototype)
         
     def create_eq(self, attr):
         def eq(self, other):
@@ -92,13 +92,13 @@ class _identity(_property):
             return self.attr.__hash__()
         return hash
 
-def _invariant(boolean_op):
+def Invariant(boolean_op):
     boolean_op.__is_an_invariant__ = True
     return boolean_op
     
-def _invariant_checked(method):
+def InvariantChecked(method):
     def invariants_checked(*args, **kwargs):
-        if isinstance(args[0], _object):
+        if isinstance(args[0], Object):
             try:
                 args[0].__ultra_invariant_checks__ = False
                 t = method(*args, **kwargs)
@@ -110,19 +110,19 @@ def _invariant_checked(method):
             return method(*args, **kwargs)
     return invariants_checked
 
-class _derived_property(_ultra_property):
+class DerivedProperty(UltraProperty):
 
     def __init__(self, func):
-        super(_derived_property, self).__init__()
+        super(DerivedProperty, self).__init__()
         self._func = func
         
     def create_property(self, attr):
-        return _descriptor(lambda instance: self._func(instance))
+        return Descriptor(lambda instance: self._func(instance))
         
-def _derived(func):
-    return _derived_property(func)
+def Derived(func):
+    return DerivedProperty(func)
 
-class _meta(type):
+class Meta(type):
 
     def __new__(cls, name, bases, dict):
 
@@ -130,9 +130,9 @@ class _meta(type):
         id = None
         invariants = []
         for k, v in dict.iteritems():
-            if isinstance(v, _ultra_property):
+            if isinstance(v, UltraProperty):
                 mods[k] = v.create_property(k)
-                if isinstance(v, _identity):
+                if isinstance(v, Identity):
                     if id is None:
                         id = (v, k)
                     else:
@@ -143,7 +143,7 @@ class _meta(type):
         if len(invariants) > 0:
             for k, v in dict.iteritems():
                 if isinstance(v, FunctionType):
-                    dict[k] = _invariant_checked(v)
+                    dict[k] = InvariantChecked(v)
                 
         if len(mods) > 0:
             dict.setdefault('__ultra__', {})
@@ -153,7 +153,7 @@ class _meta(type):
             lowest_property = min(i[0] for i in dict['__ultra__'].values())
             for k in dict['__ultra__']:
                 dict['__ultra__'][k][0] = dict['__ultra__'][k][0] - lowest_property
-            _ordered_class_attr.order = 0
+            OrderedClassAttribute.order = 0
 
         if id is not None:
             v, k = id
@@ -163,11 +163,11 @@ class _meta(type):
             
         dict['__ultra_invariants__'] = invariants
         
-        t = super(_meta, cls).__new__(cls, name, bases, dict)
+        t = super(Meta, cls).__new__(cls, name, bases, dict)
         return t
 
-class _object(object):
-    __metaclass__ = _meta
+class Object(object):
+    __metaclass__ = Meta
     
     def __ultra_do_invariant_checks__(self):
         if getattr(self, '__ultra_invariant_checks__', False):
@@ -181,221 +181,3 @@ class _object(object):
         properties.sort(key=lambda x: x[1][0])
         return [(i[0], i[1][1]) for i in properties]
                 
-if __name__ == '__main__':
-
-    import unittest
-
-    class Tests(unittest.TestCase):
-    
-        def test_property(self):
-            p = _property(int)
-                    
-        def test_object(self):
-            
-            class u(_object):
-                a = _property(int)
-                
-                def __init__(self, init_a):
-                    super(u, self).__init__()
-                    self.a = init_a
-            
-            i = u(2)
-            self.assertEqual(i.a, 2)
-            
-            i.a = 3
-            self.assertEqual(i.a, 3)
-            
-            self.assertRaises(TypeError, setattr, i, 'a', '4')
-    
-            j = u(0)
-            j.a = 4
-            self.assertEqual(j.a, 4)
-            self.assertEqual(i.a, 3)
-            
-        def test_two_properties(self):
-            
-            class u(_object):
-                b = _property(int)
-                a = _property(str)
-                
-                def __init__(self, init_a, init_b):
-                    super(u, self).__init__()
-                    self.a = init_a
-                    self.b = init_b
-                    
-            i = u('one', 1)
-            self.assertEqual(i.a, 'one')
-            self.assertEqual(i.b, 1)
-            self.assertEqual(type(i).__ultra__['a'][0], 1)
-            self.assertEqual(type(i).__ultra__['b'][0], 0)
-            
-        def test_identity(self):
-            
-            class u(_object):
-                a = _identity(str)
-                b = _property(int)
-
-                def __init__(self, init_a, init_b):
-                    super(u, self).__init__()
-                    self.a = init_a
-                    self.b = init_b
-                    
-            i = u('one', 1)
-            self.assertEqual(i.a, 'one')
-            self.assertEqual(i.b, 1)
-            self.assertEqual(type(i).__ultra__['a'][0], 0)
-            self.assertEqual(type(i).__ultra__['b'][0], 1)
-            
-            j = u('one', 2)
-            self.assertEqual(i, j)
-            j.a = 'two'
-            self.assertNotEqual(i, j)
-            
-        def test_collections(self):
-            
-            class u(_object):
-                a = _property([int])
-                
-                def __init__(self, init_a = None):
-                    super(u, self).__init__()
-                    self.a = replace_none(init_a, [])
-                        
-            i = u()
-            
-            self.assertEqual(i.a, [])
-            i.a = [1, 2, 3]
-            self.assertEqual(i.a, [1, 2, 3])
-            self.assertRaises(TypeError, setattr, i, 'a', [1, 2, 'three'])
-            i.a.append(4)
-            self.assertEqual(i.a, [1, 2, 3, 4])
-            self.assertRaises(TypeError, i.a.append, 'four')
-            i.a[1:3] = [5, 6]
-            self.assertEqual(i.a, [1, 5, 6, 4])
-            self.assertRaises(TypeError, i.a.__setslice__, 1, 3, ['one', 'two'])
-            
-        def test_mappings(self):
-            
-            class u(_object):
-                a = _property({int:str})
-                
-                def __init__(self, init_a = None):
-                    super(u, self).__init__()
-                    self.a = replace_none(init_a, {})
-                        
-            i = u()
-            
-            self.assertEqual(i.a, {})
-            i.a = { 1 : 'one', 2 : 'two' }
-            self.assertEqual(i.a, { 1 : 'one', 2 : 'two' })
-            self.assertRaises(TypeError, setattr, i, 'a', { 1 : 1.0, 2 : 2.0 })
-            i.a[3] = 'three'
-            self.assertEqual(i.a[3], 'three')
-            self.assertRaises(TypeError, i.a.__setitem__, 3, 3.0)
-            
-        def test_depth(self):
-        
-            class u(_object):
-                a = _property([[int]])
-                
-                def __init__(self, init_a = None):
-                    super(u, self).__init__()
-                    self.a = replace_none(init_a, [])
-                        
-            i = u()
-            i.a = [[1, 2], [3, 4]]
-            self.assertEqual(i.a, [[1, 2], [3, 4]])
-            self.assertRaises(TypeError, i.a.append, 4)
-            self.assertRaises(TypeError, i.a.append, ['a', 'b'])
-            
-        def test_invariant(self):
-            
-            class u(_object):
-                a = _property(int)
-                b = _property([str])
-                
-                def __init__(self, a = 0, b = None):
-                    super(u, self).__init__()
-                    self.a = a
-                    self.b = replace_none(b, [])
-                    
-                @_invariant
-                def verify(self):
-                    return len(self.b) == self.a
-                    
-                def modify(self, a, b):
-                    self.a = a
-                    self.b = b
-                    
-                @classmethod
-                def foo(cls, bar):
-                    pass
-                    
-                @staticmethod
-                def bar():
-                    pass
-                    
-            i = u()
-            i.modify(2, ['one', 'two'])
-            self.assertRaises(ValueError, setattr, i, 'a', 3)
-            i.a = 2
-            self.assertRaises(ValueError, i.b.append, 'three')
-            i.a = 3
-            i.foo(4)
-            i.bar()
-            
-        def test_derived(self):
-            
-            class u(_object):
-                b = _property([str])
-                
-                def __init__(self, b = None):
-                    super(u, self).__init__()
-                    self.b = replace_none(b, [])
-                    
-
-                @_derived
-                def a(self):
-                    return len(self.b)
-
-                @classmethod
-                def foo(cls, bar):
-                    pass
-                    
-                @staticmethod
-                def bar():
-                    pass
-                    
-            i = u(['one', 'two', 'three', 'four'])
-            self.assertEqual(i.a, 4)
-            i.foo(4)
-            i.bar()
-            
-        def test_inheritance(self):
-        
-            class u(_object):
-                a = _property(str)
-                
-                def __init__(self, a = None):
-                    super(u, self).__init__()
-                    self.a = replace_none(a, '')
-
-            class v(u):
-                b = _property(int)
-                
-                def __init__(self, a = None, b = None):
-                    super(v, self).__init__(a)
-                    self.b = replace_none(b, 0)
-                    
-            i = v('foo', 5)
-            self.assertEqual(i.a, 'foo')
-            self.assertEqual(i.b, 5)
-            self.assertRaises(TypeError, setattr, i, 'a', 3)
-            
-                    
-           
-            
-    suite = unittest.TestLoader().loadTestsFromTestCase(Tests)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    
-        
-            
