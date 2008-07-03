47f8a6b9b8acbb77ca527a61764829fa93724603
nicolas.minutillo <nicolas.minutillo@8e4a23b9-e14e-0410-9f2d-2f2a39760a57>
2008-07-03 02:04:36 +0000

    More organization.

--- ultrameta/type_definition.py
+++ ultrameta/type_definition.py
@@ -1,7 +1,8 @@
 #/usr/bin/env python
 
+from itertools import izip
 from utils import replace_none
-import containers
+import validators
 import unittest
 
 class TypeDefinitionTests(unittest.TestCase):
@@ -64,6 +65,121 @@ class TypeDefinitionTests(unittest.TestCase):
         self.assertEqual(self.mixed_type.type_match([{(1, 3) : 'onethree', (0, 1) : 'zeroone'},
             { (0, 0) : 'done' }]), True)
 
+class ContainerTests(unittest.TestCase):
+
+    def append(list, val):
+        list.append(val)
+        
+    def assign(map, key, val):
+        map[key] = val
+    
+    def test_list_of_int(self):
+        type_def = TypeDefinition([int])
+        test_list = ListProxy([1, 2, 3]).withtype(type_def)
+        self.assertEqual(test_list, [1, 2, 3])
+        self.assertEqual(test_list[1], 2)
+        self.assertEqual(len(test_list), 3)
+       
+    def test_type_safe_list_of_int(self):
+        type_def = TypeDefinition([int])
+        test_list = ListProxy([]).withtype(type_def)
+        self.assertRaises(TypeError, self.append, test_list, 'a')
+        
+    def test_map_from_str_to_int(self):
+        type_def = TypeDefinition({str:int})
+        test_map = DictionaryProxy({'a' : 1, 'b' : 2}).withtype(type_def)
+        self.assertEqual(test_map['a'], 1)
+        test_map = DictionaryProxy({'a' : 1, 'b' : 'c'})
+        self.assertRaises(TypeError, test_map.withtype, type_def)
+        
+    def test_type_safe_map_from_str_to_int(self):
+        type_def = TypeDefinition({str:int})
+        test_map = DictionaryProxy({}).withtype(type_def)
+        self.assertRaises(TypeError, self.assign, test_map, 1, 1)
+        self.assertRaises(TypeError, self.assign, test_map, 'a', 'a')
+        
+    def test_tuple_int_int_str_int(self):
+        type_def = TypeDefinition((int, int, str, int))
+        test_tuple = TupleProxy((1, 2, 'c', 3)).withtype(type_def)
+        self.assertEqual(test_tuple, (1, 2, 'c', 3))
+        self.assertEqual(test_tuple[1], 2)
+        self.assertRaises(TypeError, self.assign, test_tuple, 2, 4)
+        
+    def test_tuple_type_safety(self):
+        type_def = TypeDefinition((int, int, str, int))
+        test_tuple = TupleProxy((1, 2, 3))
+        self.assertRaises(TypeError, test_tuple.withtype, type_def)
+
+def InvariantChecked(self, func):
+    
+    def wrapper(val):
+        func(val)
+        self._invariant_checks()
+        
+    return wrapper
+    
+class WithMixin(object):
+
+    def __with(self, type_definition = None, invariant_checks = None):
+        self._type_definition = replace_none(getattr(self, '_type_definition', None), type_definition)
+        self._invariant_checks = replace_none(getattr(self, '_invariant_checks', None), invariant_checks)
+        
+        methods, proxies = self.proxy_definitions()
+        if self._type_definition:
+            for method, proxy in izip(methods, proxies):
+                setattr(self, method, proxy)
+        if self._invariant_checks:
+            for method in methods:
+                setattr(self, method, InvariantChecked(self, getattr(self, method)))
+
+        return self             
+
+    def withtype(self, type_definition):
+        if not type_definition.type_match(self):
+            raise TypeError()
+        return self.__with(type_definition = type_definition)
+        
+    def withinvariants(self, invariant_checks):
+        invariant_checks()
+        return self.__with(invariant_checks = invariant_checks)
+        
+    def proxy_definitions(self):
+        raise NotImplementedError()
+        
+class ListProxy(list, WithMixin):
+
+    def proxy_definitions(self):
+        return ['append', '__setslice__'], [self.__append_type_safe, self.__setslice_type_safe]
+
+    def __append_type_safe(self, val):
+        if self._type_definition.contents_match(val, None):
+            super(ListProxy, self).append(val)
+        else:
+            raise TypeError('%s is a %s and cannot be appended to a %s' % 
+                (val, type(val), self._type_definition))
+
+    def __setslice_type_safe(self, i, j, val):
+        if self._type_definition.type_match(val):
+            return super(ListProxy, self).__setslice__(i, j, val)
+        else:
+            raise TypeError('%s is not a %s' % (val, self._type_definition))        
+
+            
+class TupleProxy(tuple, WithMixin):
+
+    def proxy_definitions(self):
+        return [], []
+    
+class DictionaryProxy(dict, WithMixin):
+
+    def proxy_definitions(self):
+        return ['__setitem__'], [self.__setitem_type_safe]
+
+    def __setitem_type_safe(self, key, val):
+        if self._type_definition.contents_match(val, key):
+            super(DictionaryProxy, self).__setitem__(key, val)
+        else:
+            raise TypeError('%s is not a %s' % ((key, val), self._type_definition))
 
 leaf_name = 'leaf'
 tuple_name = 'tuple'
@@ -72,9 +188,9 @@ mapping_name = 'mapping'
 
 container_definitions = {  
     type(None) : (None, leaf_name) ,
-    tuple : (containers.TupleProxy, tuple_name), 
-    list : (containers.ListProxy, sequence_name),
-    dict : (containers.DictionaryProxy, mapping_name) }
+    tuple : (TupleProxy, tuple_name), 
+    list : (ListProxy, sequence_name),
+    dict : (DictionaryProxy, mapping_name) }
     
 type_match_definitions = {
     leaf_name: lambda self, val: self._type is None or isinstance(val, self._type),
@@ -168,4 +284,9 @@ class TypeDefinition(object):
         return self._type_repr()
     
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(unittest.TestLoader().loadTestsFromTestCase(TypeDefinitionTests))        
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(TypeDefinitionTests)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(ContainerTests)
+    master = unittest.TestSuite()
+    master.addTests((suite1, suite2))
+    unittest.TextTestRunner(verbosity=2).run(master) 
+    
